b15e4c5579bbe8a51cf6385e77b32264ecc5c20e
KsrKbt <134358071+KsrKbt@users.noreply.github.com>
2024-03-12 15:41:12 +0900

    scan doors

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 import time
-import os  
+import os
+import sys
 #import datetime
 #from datetime import datetime
 #from PIL import Image
@@ -183,7 +184,7 @@ def main_func(img,):
       #return (scores+'  time_sec : '+str(time.time()-t1))
       return input_image ,scores
 
-
+'''
 import gradio as gr
 import cv2
 import os 
@@ -215,3 +216,42 @@ demo_app = gr.Interface(examples=["s.jpg"],
     cache_examples=True,
 )
 demo_app.launch()
+'''
+'''
+args = sys.argv
+im=cv2.imread(args[1])
+im,number=main_func(im)
+#im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+#print(number)
+cv2.imwrite("output.jpg", im)
+'''
+
+# カメラ映像からVideoCaptureオブジェクトを取得
+cap = cv2.VideoCapture(0)
+# カメラに問題がないかの確認
+if not cap.isOpened():
+    print("カメラが正常ではありません")
+    exit()
+# カメラから連続的にキャプチャ画像を取得
+while True:
+    # Bool値とキャプチャ画像を変数に格納
+    ret, frame = cap.read()
+    # キャプチャ画像が正しく読み込めたかの確認
+    if not ret:
+        print("画像を正しく読み込めませんでした")
+        break
+    # 画像データをグレースケール化（白黒）
+    #frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # 顔を検出する
+    #lists = cascade.detectMultiScale(frame_gray, minSize=(50, 50))
+    # forですべての顔を赤い長方形で囲む
+    #for (x,y,w,h) in lists:
+    #    cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), thickness=2)
+    frame,number=main_func(frame)
+    # 顔認識を行っている画像を表示
+    cv2.imshow('video image', frame)
+    # qが押されたらwhileから抜ける
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
