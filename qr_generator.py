f58e210aa7c3e882066dba3b2c421de5e43a145e
KsrKbt <134358071+KsrKbt@users.noreply.github.com>
2024-04-05 19:39:39 +0900

    部屋識別用QRコード作成

--- qr_generator.py
+++ qr_generator.py
@@ -5,15 +5,27 @@ FILE_PNG_B = 'qrcode_B.png'
 FILE_PNG_C = 'qrcode_C.png'
 
 # QRコード作成
-code = pyqrcode.create('https://qiita.com/PoodleMaster', error='L', version=3, mode='binary')
-code.png(FILE_PNG_A, scale=5, module_color=[0, 0, 0, 128], background=[255, 255, 255])
+# code = pyqrcode.create('https://qiita.com/PoodleMaster', error='L', version=3, mode='binary')
+#code.png(FILE_PNG_A, scale=5, module_color=[0, 0, 0, 128], background=[255, 255, 255])
 
 # QRコード作成
-code = pyqrcode.create('https://github.com/PoodleMaster', error='L', version=3, mode='binary')
-code.png(FILE_PNG_B, scale=5, module_color=[0, 0, 0, 128], background=[255, 255, 255])
+#code = pyqrcode.create('https://github.com/PoodleMaster', error='L', version=3, mode='binary')
+#code.png(FILE_PNG_B, scale=5, module_color=[0, 0, 0, 128], background=[255, 255, 255])
 
 # QRコードに埋め込む文字列
 encoded_text = "room1"
+# QRコードを生成
+code = pyqrcode.create(encoded_text)
+code.png(FILE_PNG_A, scale=5, module_color=[0, 0, 0, 128], background=[255, 255, 255])
+
+# QRコードに埋め込む文字列
+encoded_text = "room2"
+# QRコードを生成
+code = pyqrcode.create(encoded_text)
+code.png(FILE_PNG_B, scale=5, module_color=[0, 0, 0, 128], background=[255, 255, 255])
+
+# QRコードに埋め込む文字列
+encoded_text = "room3"
 # QRコードを生成
 code = pyqrcode.create(encoded_text)
 code.png(FILE_PNG_C, scale=5, module_color=[0, 0, 0, 128], background=[255, 255, 255])
